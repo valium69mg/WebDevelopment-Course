cacae41a01200c484b4d698675bf9743e7a21e38
Carlos PC <xdeus.v2@gmail.com>
2024-02-07 09:59:23 -0600

    Modified the code to render login using terneary operator and added padding to de login form file on css

--- Code/ReactConditionalRendering/my-app/src/App.jsx
+++ Code/ReactConditionalRendering/my-app/src/App.jsx
@@ -3,21 +3,14 @@ import './App.css';
 import Login from './Login';
 
 let isLoggedIn = false;
-let user = "user";
-
-
-function RenderConditionally(){
-  if (isLoggedIn === true){
-    return <h1> {`Hello ${user}.`} </h1> ;
-    } else {
-    return <Login/>;
-    }
-  }
+let user = "User";
 
 function App() {
   return (
-    <div className="App">
-      <RenderConditionally/>
+    <div className="App">{
+      isLoggedIn === true ? 
+      <h1> Hello {user}. </h1>:<Login/>
+      }
     </div>
   );
 }
