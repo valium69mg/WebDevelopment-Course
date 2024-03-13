08cb060a03f0da093ba8fe8a3ba67bc4471a432e
Carlos PC <xdeus.v2@gmail.com>
2024-03-12 20:18:02 -0600

    Added env variables and api key auth to the get all users endpoint

--- web store/store_server/index.js
+++ web store/store_server/index.js
@@ -2,6 +2,9 @@ import express from "express";
 import cors from 'cors';
 import bodyParser from "body-parser";
 
+// ENVIROMENT VARIABLES
+import 'dotenv/config';
+
 //USERS
 import { searchUser } from "./tools/database.js";
 
@@ -26,10 +29,19 @@ app.get('/', (req,res) => {
 });
 
 // USERS 
+
 // GET ALL USERS
 app.get("/users/all", async (req,res) => {
-    const all_users = await searchUser("all");
-    res.json(all_users);
+    // WE WILL NEED AN API-KEY AUTH FOR THIS ROUTE
+    const user_apikey = req.headers['api-key']; // &auth-key=*********
+    const dotenv_apikey = process.env.API_KEY;
+    if (user_apikey === dotenv_apikey) {
+        const all_users = await searchUser("all");
+        res.json(all_users);
+    } else {
+        res.sendStatus(401);
+    }
+    
 });
 // CREATE USERS
 
