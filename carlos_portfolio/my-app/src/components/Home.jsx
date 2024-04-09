8599548f7a37099513cb53385bf491f084e2f1ce
valium69mg <xdeus.v2@gmail.com>
2024-04-08 18:36:58 -0600

    Corrected styling in the home page

--- carlos_portfolio/my-app/src/components/Home.jsx
+++ carlos_portfolio/my-app/src/components/Home.jsx
@@ -24,7 +24,7 @@ const AnimatedHome = () => {
         >
             <PresentationCard/>
 
-            <Logos/>
+            
             
         </motion.div>
     );
--- carlos_portfolio/my-app/src/components/PresentationCard.jsx
+++ carlos_portfolio/my-app/src/components/PresentationCard.jsx
@@ -1,18 +1,22 @@
 import React from "react";
 import image from "./img/carlos.png";
 import './styles/presentationCardStyles.css';
+import Logos from "./Logos";
+
 
 function PresentationCard(){
     return (
         <div className="presentationCard">
-            <div className="wrapper">
+        <div className="wrapper">
                 <img src={image} alt="carlos"/>
-            </div> 
-            <div className="presentationCard-paragraph">
+            </div>     
+        <div className="presentationCard-paragraph">
                 <h1> Hi, I am Carlos.</h1>
                 <p> I'm a Engineer, graduated from Tec de Monterrey. I focus my work on Web and App development, using various languages and frameworks.  </p>
                 <a href="/Projects"> See my work </a>
+                <Logos/>
             </div>
+              
         </div>
     )
 }
